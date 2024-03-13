9cbb5211142930f2787ebeb1c14dd33a3daf315f
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-13 17:14:30 +0000

    debug: add debug server for websockets

---
+++ debug_server.py
@@ -0,0 +1,31 @@
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosed
+
+client_id = 0
+
+async def test(websocket, path):
+    global client_id
+    client_id += 1
+    current_client_id = client_id
+    print(f"New client connected: {current_client_id}")
+    iterateur = 0
+    try:
+        while True:
+            await websocket.send(str(iterateur))
+            await asyncio.sleep(1)
+            print(iterateur)
+            iterateur += 1
+    except ConnectionClosed:
+        print(f"Client {current_client_id} disconnected")
+
+async def main():
+    async with websockets.serve(test, "localhost", 8765):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    try:
+        print("Server started on: ws://localhost:8765. Press Ctrl+C to stop.\n")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nServer stopped by user.")
